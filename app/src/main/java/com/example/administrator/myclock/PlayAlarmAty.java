bd2968cc8fea3ad8e0f1479646e961dfe16d591e
liukqsmy <liukqsmy@sina.com>
2016-05-30 20:45:55 +0800

    呈现闹钟界面

---
+++ app/src/main/java/com/example/administrator/myclock/PlayAlarmAty.java
@@ -0,0 +1,13 @@
+package com.example.administrator.myclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayAlarmAty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_alarm_aty);
+    }
+}
