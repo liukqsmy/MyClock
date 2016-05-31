0863ecbb712244e2bc8ea32a12df083e3003691d
liukqsmy <liukqsmy@sina.com>
2016-05-31 09:13:07 +0800

    闹钟执行，播放音乐

--- app/src/main/java/com/example/administrator/myclock/PlayAlarmAty.java
+++ app/src/main/java/com/example/administrator/myclock/PlayAlarmAty.java
@@ -1,5 +1,6 @@
 package com.example.administrator.myclock;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,25 @@ public class PlayAlarmAty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_alarm_aty);
+
+        mp = MediaPlayer.create(this, R.raw.alarm);
+        mp.start();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mp.stop();
+        mp.release();
+    }
+
+    private MediaPlayer mp;
 }
